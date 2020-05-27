a75302538c26de37a0bd285eea8d181ede294463
ellie <ellie.tms1371@gmail.com>
2020-05-27 16:43:16 +0100

    add responsive changes

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -7,14 +7,14 @@ const HighScore = (props) => {
         <h1>High Scores Per Country </h1>
     { props.CountryScores.sort((a, b) => (a.name > b.name) ? 1 : -1).map((eachCountry,index) =>
     <Table className="countryBorder" bordered key={index}>
-        <tbody className="tableBody col-2 col-lg-12">
+        <tbody className="container">
         <tr> 
-        <td>
-            <h1 className="score"><span>High Scores:</span><span className="countryName">{eachCountry.name}</span></h1>
+        <td className="score">
+            <h1><span className="header">High Scores:</span><span className="countryName">{eachCountry.name}</span></h1>
         </td>
         </tr>
-        <tr className="ml-3" >
-        <td>
+        <tr>
+        <td className="small-table">
             {eachCountry.scores.sort((a, b) => (a.s > b.s) ? -1 : 1).map((element,i) => 
             <Table bordered className="scoreClass" key={i}>
             <tbody className="bodyTable">
@@ -29,7 +29,7 @@ const HighScore = (props) => {
                 )
             }
             </td>    
-            </tr>
+         </tr>
         </tbody>
         </Table>
     )
