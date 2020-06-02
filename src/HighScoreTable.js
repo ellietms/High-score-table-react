0344d6ee610a3805f6395b52389cdb3a0b5b2014
ellie <ellie.tms1371@gmail.com>
2020-06-02 17:25:39 +0100

    make component for page

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,40 +1,12 @@
-import React from 'react';
-import {Table} from 'reactstrap';
-
+import React,{useState} from 'react';
+import Page  from './Table'
 const HighScore = (props) => {
+    const [Asending,setAsending] = useState(false)
+    function handleAsending (){
+        setAsending(!Asending);
+    } 
     return(
-    <div className="App col-12">
-        <h1>High Scores Per Country </h1>
-    { props.CountryScores.sort((a, b) => (a.name > b.name) ? 1 : -1).map((eachCountry,index) =>
-    <Table className="countryBorder" bordered key={index}>
-        <tbody className="container">
-        <tr> 
-        <td className="score">
-            <h1><span className="header">High Scores:</span><span className="countryName">{eachCountry.name}</span></h1>
-        </td>
-        </tr>
-        <tr>
-        <td className="small-table">
-            {eachCountry.scores.sort((a, b) => (a.s > b.s) ? -1 : 1).map((element,i) => 
-            <Table bordered className="scoreClass" key={i}>
-            <tbody className="bodyTable">
-            <tr> 
-                <td className="myName">{element.n.toUpperCase()}</td>
-            </tr>
-            <tr>    
-                <td className="myScore">{element.s}</td>
-            </tr>
-            </tbody>
-                </Table>
-                )
-            }
-            </td>    
-         </tr>
-        </tbody>
-        </Table>
-    )
-}  
-</div>
+   <Page onClick={handleAsending} data={props} />
 )
 }
 
---
+++ src/Table.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Table} from 'reactstrap';
+
+const Page = (props) => {
+return( 
+    <div className="App col-12">
+    <button onClick={props.handleAsending} type="button" class="btn btn-primary">
+     
+    </button>  
+    <h1>High Scores Per Country </h1>
+    { props.data.CountryScores.sort((a, b) => (a.name > b.name) ? 1 : -1).map((eachCountry,index) =>
+    <Table className="countryBorder" bordered key={index}>
+        <tbody className="container">
+        <tr> 
+        <td className="score">
+            <h1><span className="header">High Scores:</span><span className="countryName">{eachCountry.name}</span></h1>
+        </td>
+        </tr>
+        <tr>
+        <td className="small-table">
+            {eachCountry.scores.sort((a, b) => (a.s > b.s) ? -1 : 1).map((element,i) => 
+            <Table bordered className="scoreClass" key={i}>
+            <tbody className="bodyTable">
+            <tr> 
+                <td className="myName">{element.n.toUpperCase()}</td>
+            </tr>
+            <tr>    
+                <td className="myScore">{element.s}</td>
+            </tr>
+            </tbody>
+                </Table>
+                )
+            }
+            </td>    
+         </tr>
+        </tbody>
+        </Table>
+    )
+}  
+    </div>
+    );
+}
+
+export default Page;
+
+ 
